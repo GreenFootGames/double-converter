d499c49d34c4cb1e8ccab89491215b52f70d3009
Ákos Csenki <akos.csenkii@gmail.com>
2022-10-27 19:30:30 +0100

    Performance optimizations and bug fixes.

--- InfiniteDecimalConv/Program.cs
+++ InfiniteDecimalConv/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace InfiniteDecimalConv
@@ -10,54 +11,63 @@ namespace InfiniteDecimalConv
     {
         static void Main(string[] args)
         {
-            while(true)
+            double n;
+            string input, intervalStartN, intervalEndN, afterDecimal, interval;
+            int intervalStartIndex, timesX, minN, intervalEndIndex, timesY, maxN;
+
+            while (true)
             {
+                
                 Console.WriteLine("Please enter the infinite decimal:");
-                string input = Console.ReadLine();
+                input = Console.ReadLine();
+
                 Console.WriteLine("Enter the start of the infinite segment:");
-                string inputRepeatingN = Console.ReadLine();
+                intervalStartN = Console.ReadLine();
+
                 Console.WriteLine("Enter the end of the infinite segment: (if there is one repeating num, then write num here)");
-                string inputLastRepeatingN = Console.ReadLine();
-                double n, firstNToRepeat, lastNOfInterval;
+                intervalEndN = Console.ReadLine();
 
-                try
-                {
-                    n = double.Parse(input);
-                    firstNToRepeat = int.Parse(inputRepeatingN);
-                    lastNOfInterval = int.Parse(inputLastRepeatingN);
+                
 
+                if(double.TryParse(input, out n) && n % 1 != 0)
+                {                  
+                    afterDecimal = input.Substring(input.IndexOf('.') + 1);
+                    intervalStartIndex = afterDecimal.IndexOf(intervalStartN[0]);
+                    
+                    if(intervalStartIndex < 0)
+                    {
+                        Console.WriteLine("Invalid data provided!");
+                        continue;
+                    }
 
+                    interval = afterDecimal.Substring(intervalStartIndex);
+                    timesX = (int) Math.Pow(10, intervalStartIndex);
+                    minN = (int) (n * timesX);       
+                    intervalEndIndex = interval.IndexOf(intervalEndN[0]);
 
-                    int decimalPoint = input.IndexOf(',');
-                    // Console.WriteLine("Decimal point index: " + decimalPoint);
-                    string afterDecimal = input.Remove(0, decimalPoint + 1);
-                    // Console.WriteLine("AfterDecimal: " + afterDecimal);
+                    if (intervalEndIndex < 0)
+                    {
+                        Console.WriteLine("Invalid data provided!");
+                        continue;
+                    }
 
+                    timesY = (int) Math.Pow(10, intervalStartIndex + intervalEndIndex + 1);
+                    maxN = (int)(n * timesY);
 
+                    //Console.WriteLine("Decimal point index: " + decimalPoint);
+                    //Console.WriteLine("AfterDecimal: " + afterDecimal);
+                    //Console.WriteLine("TimesY: " + timesY);
+                    //Console.WriteLine("Times X: " + timesX);
+                    //Console.WriteLine("MaxY: " + (int)maxN);
+                    //Console.WriteLine("MinX: " + minN);
 
-                    int infinityBorder = afterDecimal.IndexOf(inputRepeatingN);
-                    int timesX = (int)Math.Pow(10, infinityBorder);
-                    // Console.WriteLine("Times X: " + timesX);
-                    double minN = n * timesX;
-                    Console.WriteLine("MinN: " + minN);
-
-
-
-                    int endOfInterval = afterDecimal.IndexOf(inputLastRepeatingN);
-                    double timesY = Math.Pow(10, endOfInterval + 1);
-                    double maxN = n * timesY;
-                    // Console.WriteLine("TimesY: " + timesY);
-                    Console.WriteLine("MaxN: " + (int)maxN);
-
-
-                    // Even though maxN is theoretically always int, converting it manages if the user made a misinput
-                    Console.WriteLine("The division form is: " + ((int)maxN - (int)minN) + "/" + (timesY - timesX));
+                    Console.WriteLine("The division form is: " + (maxN - minN) + "/" + (timesY - timesX));          
 
                 }
-                catch (System.Exception e)
+                else
                 {
 
-                    Console.WriteLine("Never gonna give you up, never gonna let you down.\nNever gonna run around and desert you.\nNever gonna make you cry, never gonna say goodbye.\n" + e);
+                    Console.WriteLine("\nNever gonna give you up, never gonna let you down.\nNever gonna run around and desert you.\nNever gonna make you cry, never gonna say goodbye.");
                 }
 
                 Console.ReadKey();
